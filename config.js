4fe22552521eaff960a2e5023959d2c3cc0707b3
Tyler Harper <tylerdh12@gmail.com>
2020-07-03 21:09:35 -0700

    Setup config for fetch and setup basic of opp

---
+++ config.js
@@ -0,0 +1,13 @@
+import {
+  AUTH_DOMAIN,
+  CLIENT_ID,
+  FAUNA_SECRET_KEY,
+  WEATHER_SECRET_KEY,
+} from "react-native-dotenv";
+
+export default {
+  FAUNA_SECRET_KEY,
+  AUTH_DOMAIN,
+  CLIENT_ID,
+  WEATHER_SECRET_KEY,
+};
--- src/Stacks/MyGardenStack.tsx
+++ src/Stacks/MyGardenStack.tsx
@@ -1,23 +1,158 @@
 import { Ionicons } from "@expo/vector-icons";
 import { createStackNavigator } from "@react-navigation/stack";
-import faker from "faker";
-import React from "react";
-import { FlatList, Text, View } from "react-native";
-import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { addProductRoutes } from "../addProductRoutes";
 import {
   MyGardenParamList,
   MyGardenStackNavProps,
 } from "../ParamLists/MyGardenParamList";
-import { Center } from "../StyledContainers/Center";
+import config from "./../../config";
 
 interface MyGardenStackProps {}
 const Stack = createStackNavigator<MyGardenParamList>();
 
 function MyGarden({ navigation }: MyGardenStackNavProps<"MyGarden">) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post(
+        "https://graphql.fauna.com/graphql",
+        {
+          query: `
+          query {
+            allPlants {
+              data {
+                _id
+                datePlanted
+                daysToHarvest
+                daysToGerminate
+                variety
+                species
+                plantingMonths
+              }
+            }
+          }
+        `,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${config.FAUNA_SECRET_KEY}`,
+          },
+        }
+      )
+      .then((res) => {
+        setData(res.data.data.allPlants.data);
+      })
+      .catch((error) => alert(error))
+      .finally(setLoading(false));
+  }, []);
+
   return (
-    <Center>
-      <ScrollView
+    <SafeAreaView>
+      {isLoading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <View>
+          <FlatList
+            style={{
+              width: "100%",
+            }}
+            renderItem={({ item }) => {
+              return (
+                <TouchableOpacity
+                  style={{
+                    justifyContent: "center",
+                    padding: 30,
+                    margin: 12,
+                    backgroundColor: "rgb(251, 252, 252)",
+                    borderRadius: 30,
+                    shadowColor: "#000",
+                    shadowOffset: { width: 6, height: 5 },
+                    shadowOpacity: 0.2,
+                    shadowRadius: 4,
+                    elevation: 5,
+                  }}
+                  onPress={() => {
+                    navigation.navigate("Product", { name: item.species });
+                  }}
+                >
+                  <Text style={{ fontSize: 18, fontWeight: "600" }}>
+                    {item.species}
+                  </Text>
+                  <View
+                    style={{
+                      marginTop: 10,
+                      backgroundColor: "transparent",
+                      borderStyle: "solid",
+                      borderWidth: 1,
+                      borderColor: "rgb(148, 224, 136)",
+                      borderRadius: 10,
+                      width: "100%",
+                    }}
+                  >
+                    <View
+                      style={{
+                        height: 8,
+                        backgroundColor: "rgb(148, 224, 136)",
+                        borderRadius: 10,
+                        width: "20%",
+                        flex: 1,
+                      }}
+                    ></View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ width: "50%" }}>
+                      <Text
+                        style={{
+                          textAlign: "left",
+                          marginTop: 10,
+                          marginBottom: 5,
+                          fontWeight: "500",
+                        }}
+                      >
+                        Date Planted
+                      </Text>
+                      <Text style={{ textAlign: "left" }}>
+                        {item.datePlanted}
+                      </Text>
+                    </View>
+                    <View style={{ width: "50%" }}>
+                      <Text
+                        style={{
+                          textAlign: "right",
+                          marginTop: 10,
+                          marginBottom: 5,
+                          fontWeight: "500",
+                        }}
+                      >
+                        Days Until Harvest
+                      </Text>
+                      <Text style={{ textAlign: "right" }}>
+                        {item.daysToHarvest}
+                      </Text>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              );
+            }}
+            keyExtractor={(product, idx) => product + idx}
+            data={data}
+          />
+        </View>
+      )}
+      {/* <ScrollView
         style={{
           width: "100%",
         }}
@@ -128,8 +263,8 @@ function MyGarden({ navigation }: MyGardenStackNavProps<"MyGarden">) {
           keyExtractor={(product, idx) => product + idx}
           data={Array.from(Array(50), () => faker.commerce.product())}
         />
-      </ScrollView>
-    </Center>
+        </ScrollView> */}
+    </SafeAreaView>
   );
 }
 
