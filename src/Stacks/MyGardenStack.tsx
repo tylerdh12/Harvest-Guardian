c05de308400b391d08f52b53091b1643df2df56a
Tyler Harper <tylerdh12@gmail.com>
2020-07-06 15:43:56 -0700

    pass id to plant details

--- src/Stacks/MyGardenStack.tsx
+++ src/Stacks/MyGardenStack.tsx
@@ -15,6 +15,7 @@ import {
   MyGardenParamList,
   MyGardenStackNavProps,
 } from "../ParamLists/MyGardenParamList";
+import { Center } from "../StyledContainers/Center";
 import config from "./../../config";
 
 interface MyGardenStackProps {}
@@ -23,7 +24,6 @@ const Stack = createStackNavigator<MyGardenParamList>();
 function MyGarden({ navigation }: MyGardenStackNavProps<"MyGarden">) {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [title, setTitle] = useState([]);
 
   useEffect(() => {
     axios
@@ -62,7 +62,9 @@ function MyGarden({ navigation }: MyGardenStackNavProps<"MyGarden">) {
   return (
     <SafeAreaView>
       {isLoading ? (
-        <ActivityIndicator size="large" />
+        <Center>
+          <ActivityIndicator size="large" />
+        </Center>
       ) : (
         <View>
           <FlatList
@@ -85,7 +87,10 @@ function MyGarden({ navigation }: MyGardenStackNavProps<"MyGarden">) {
                     elevation: 5,
                   }}
                   onPress={() => {
-                    navigation.navigate("Product", { name: item.species });
+                    navigation.navigate("Product", {
+                      name: item.species,
+                      plantId: item._id,
+                    });
                   }}
                 >
                   <Text style={{ fontSize: 18, fontWeight: "600" }}>
@@ -152,118 +157,6 @@ function MyGarden({ navigation }: MyGardenStackNavProps<"MyGarden">) {
           />
         </View>
       )}
-      {/* <ScrollView
-        style={{
-          width: "100%",
-        }}
-      >
-        <View
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            padding: 10,
-          }}
-        >
-          <Text
-            style={{
-              fontWeight: "700",
-              fontSize: 14,
-              color: "rgb(85, 150, 224)",
-            }}
-          >
-            Current Weather
-          </Text>
-          <Text
-            style={{
-              fontSize: 18,
-              fontWeight: "800",
-              marginTop: 8,
-              color: "rgb(83, 151, 221)",
-            }}
-          >
-            43{" \u2109"}
-          </Text>
-        </View>
-        <FlatList
-          style={{
-            width: "100%",
-          }}
-          renderItem={({ item }) => {
-            return (
-              <TouchableOpacity
-                style={{
-                  justifyContent: "center",
-                  padding: 30,
-                  margin: 12,
-                  backgroundColor: "rgb(251, 252, 252)",
-                  borderRadius: 30,
-                  shadowColor: "#000",
-                  shadowOffset: { width: 6, height: 5 },
-                  shadowOpacity: 0.2,
-                  shadowRadius: 4,
-                  elevation: 5,
-                }}
-                onPress={() => {
-                  navigation.navigate("Product", { name: item });
-                }}
-              >
-                <Text style={{ fontSize: 18, fontWeight: "600" }}>{item}</Text>
-                <View
-                  style={{
-                    marginTop: 10,
-                    backgroundColor: "transparent",
-                    borderStyle: "solid",
-                    borderWidth: 1,
-                    borderColor: "rgb(148, 224, 136)",
-                    borderRadius: 10,
-                    width: "100%",
-                  }}
-                >
-                  <View
-                    style={{
-                      height: 8,
-                      backgroundColor: "rgb(148, 224, 136)",
-                      borderRadius: 10,
-                      width: "20%",
-                      flex: 1,
-                    }}
-                  ></View>
-                </View>
-                <View style={{ flexDirection: "row" }}>
-                  <View style={{ width: "50%" }}>
-                    <Text
-                      style={{
-                        textAlign: "left",
-                        marginTop: 10,
-                        marginBottom: 5,
-                        fontWeight: "500",
-                      }}
-                    >
-                      Date Planted
-                    </Text>
-                    <Text style={{ textAlign: "left" }}>June 14, 2020</Text>
-                  </View>
-                  <View style={{ width: "50%" }}>
-                    <Text
-                      style={{
-                        textAlign: "right",
-                        marginTop: 10,
-                        marginBottom: 5,
-                        fontWeight: "500",
-                      }}
-                    >
-                      Days Until Harvest
-                    </Text>
-                    <Text style={{ textAlign: "right" }}>20</Text>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            );
-          }}
-          keyExtractor={(product, idx) => product + idx}
-          data={Array.from(Array(50), () => faker.commerce.product())}
-        />
-        </ScrollView> */}
     </SafeAreaView>
   );
 }
