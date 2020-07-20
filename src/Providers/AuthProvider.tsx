500ceb5044e83ebcc0adb3e896286e06a744a174
Tyler Harper <tylerdh12@gmail.com>
2020-07-20 12:12:10 -0700

    provider to capture user info to make further reqs

--- src/Providers/AuthProvider.tsx
+++ src/Providers/AuthProvider.tsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { encode } from "base-64";
 import React, { useState } from "react";
 import { AsyncStorage } from "react-native";
 
@@ -5,9 +7,14 @@ type User = null | { username: string };
 
 export const AuthContext = React.createContext<{
   user: User;
+  authBasic: (username, password) => void;
   login: (username, password) => void;
   logout: () => void;
 }>({
+  authBasic: {
+    username: null,
+    password: null,
+  },
   user: null,
   login: () => {},
   logout: () => {},
@@ -17,16 +24,35 @@ interface AuthProviderProps {}
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User>(null);
+  const [authBasic, setAuthBasic] = useState<AuthBasic>(null);
+
+  async function getUserData(User) {
+    setAuthBasic(User);
+    const token = encode(`${User.username}:${User.password}`);
+    const auth = "Basic " + token;
+
+    await axios({
+      method: "get",
+      url: "https://harvestguardian-rest-api.herokuapp.com/v1/user",
+      headers: {
+        Authorization: auth,
+      },
+    })
+      .then((res) => setUser(res.data))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <AuthContext.Provider
       value={{
         user,
+        authBasic,
         login: (username, password) => {
           const User = {
             username,
             password,
           };
-          setUser(User);
+          getUserData(User);
           AsyncStorage.setItem("user", JSON.stringify(User));
         },
         logout: () => {
