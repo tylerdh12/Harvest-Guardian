9c400d279636f7dd29df913d4d891e121348f401
Tyler Harper <tylerdh12@gmail.com>
2020-08-03 08:05:50 -0700

    add confitional display for details

--- src/Stacks/MyGardenStack.tsx
+++ src/Stacks/MyGardenStack.tsx
@@ -311,23 +311,33 @@ function Details({ route, navigation }) {
           dataText={data.date_planted.slice(0, 10)}
         />
         <DetailListItem label="Description" dataText={data.seed.description} />
-        <DetailListItem
-          label="Days To Germinate"
-          dataText={data.seed.days_to_germinate}
-        />
-        <DetailListItem
-          label="Days To Harvest"
-          dataText={data.seed.days_to_harvest}
-        />
-        <DetailListItem
-          label="Anti-Companion Plants"
-          dataText={data.seed.non_companions.join(", ")}
-        />
-        <DetailListItem label="Sun Requirements" dataText={data.seed.sun} />
-        <DetailListItem
-          label="Soil Temperature High"
-          dataText={data.seed.soil_temp_high}
-        />
+        {data.seed.days_to_germinate ? (
+          <DetailListItem
+            label="Days To Germinate"
+            dataText={data.seed.days_to_germinate}
+          />
+        ) : null}
+        {data.seed.days_to_harvest ? (
+          <DetailListItem
+            label="Days To Harvest"
+            dataText={data.seed.days_to_harvest}
+          />
+        ) : null}
+        {data.seed.non_companions ? (
+          <DetailListItem
+            label="Anti-Companion Plants"
+            dataText={data.seed.non_companions.join(", ")}
+          />
+        ) : null}
+        {data.seed.sun ? (
+          <DetailListItem label="Sun Requirements" dataText={data.seed.sun} />
+        ) : null}
+        {data.seed.soil_temp_high ? (
+          <DetailListItem
+            label="Soil Temperature High"
+            dataText={data.seed.soil_temp_high}
+          />
+        ) : null}
         {data.seed.sow_indoor !== "" ? (
           <DetailListItem
             label="Sowing Indoor"
@@ -340,32 +350,61 @@ function Details({ route, navigation }) {
             dataText={data.seed.sow_outdoor}
           />
         ) : null}
-        <DetailListItem label="Plant Height" dataText={data.seed.height} />
-        <DetailListItem label="Seed Depth" dataText={data.seed.depth} />
-        <DetailListItem label="Seed Spacing" dataText={data.seed.spacing} />
-        <DetailListItem label="Water Requirements" dataText={data.seed.water} />
-        <DetailListItem
-          label="Planting Months"
-          dataText={data.seed.zone._8b.join(", ")}
-        />
-        <DetailListItem
-          label="Nutrient Requirements"
-          dataText={data.seed.nutrient.join(", ")}
-        />
-        <DetailListItem
-          label="Soil Temperature Low"
-          dataText={data.seed.soil_temp_low}
-        />
-        <DetailListItem
-          label="Byproduct"
-          dataText={data.seed.byproducts.join(", ")}
-        />
-        <DetailListItem
-          label="Companion Plants"
-          dataText={data.seed.companions.join(", ")}
-        />
-        <DetailListItem label="Images" dataText={data.seed.images} />
-        <DetailListItem label="Complete Data" dataText={data.seed.complete} />
+        {data.seed.height ? (
+          <DetailListItem label="Plant Height" dataText={data.seed.height} />
+        ) : null}
+        {data.seed.depth ? (
+          <DetailListItem label="Seed Depth" dataText={data.seed.depth} />
+        ) : null}
+        {data.seed.spacing ? (
+          <DetailListItem label="Seed Spacing" dataText={data.seed.spacing} />
+        ) : null}
+        {data.seed.water ? (
+          <DetailListItem
+            label="Water Requirements"
+            dataText={data.seed.water}
+          />
+        ) : null}
+        {data.seed.zone._8b ? (
+          <DetailListItem
+            label="Planting Months"
+            dataText={data.seed.zone._8b.join(", ")}
+          />
+        ) : null}
+        {data.seed.nutrient ? (
+          <DetailListItem
+            label="Nutrient Requirements"
+            dataText={data.seed.nutrient.join(", ")}
+          />
+        ) : null}
+        {data.seed.soil_temp_low ? (
+          <DetailListItem
+            label="Soil Temperature Low"
+            dataText={data.seed.soil_temp_low}
+          />
+        ) : null}
+        {data.seed.byproducts ? (
+          <DetailListItem
+            label="Byproduct"
+            dataText={data.seed.byproducts.join(", ")}
+          />
+        ) : null}
+        {data.seed.companions ? (
+          <DetailListItem
+            label="Companion Plants"
+            dataText={data.seed.companions.join(", ")}
+          />
+        ) : null}
+        {data.seed.complete === true ? (
+          <DetailListItem label="Complete Data" dataText="Yes" />
+        ) : (
+          <DetailListItem label="Complete Data" dataText="No" />
+        )}
+        {data.seed.public === true ? (
+          <DetailListItem label="Public Seed" dataText="Yes" />
+        ) : (
+          <DetailListItem label="Public Seed" dataText="No" />
+        )}
         <View
           style={{
             justifyContent: "space-evenly",
--- src/Stacks/SeedLibraryStack.tsx
+++ src/Stacks/SeedLibraryStack.tsx
@@ -258,57 +258,98 @@ function Details({ route, navigation }) {
           marginTop: -30,
         }}
       >
-        <DetailListItem label="Variety" dataText={data.variety} />
-        <DetailListItem label="Description" dataText={data.description} />
-        <DetailListItem
-          label="Days To Germinate"
-          dataText={data.days_to_germinate}
-        />
-        <DetailListItem
-          label="Days To Harvest"
-          dataText={data.days_to_harvest}
-        />
-        <DetailListItem
-          label="Anti-Companion Plants"
-          dataText={data.non_companions.join(", ")}
-        />
-        <DetailListItem label="Sun Requirements" dataText={data.sun} />
-        <DetailListItem
-          label="Soil Temperature High"
-          dataText={data.soil_temp_high}
-        />
+        {data.variety ? (
+          <DetailListItem label="Variety" dataText={data.variety} />
+        ) : null}
+        {data.description ? (
+          <DetailListItem label="Description" dataText={data.description} />
+        ) : null}
+        {data.days_to_germinate ? (
+          <DetailListItem
+            label="Days To Germinate"
+            dataText={data.days_to_germinate}
+          />
+        ) : null}
+        {data.days_to_harvest ? (
+          <DetailListItem
+            label="Days To Harvest"
+            dataText={data.days_to_harvest}
+          />
+        ) : null}
+        {data.non_companions ? (
+          <DetailListItem
+            label="Anti-Companion Plants"
+            dataText={data.non_companions.join(", ")}
+          />
+        ) : null}
+        {data.sun ? (
+          <DetailListItem label="Sun Requirements" dataText={data.sun} />
+        ) : null}
+        {data.soil_temp_high ? (
+          <DetailListItem
+            label="Soil Temperature High"
+            dataText={data.soil_temp_high}
+          />
+        ) : null}
         {data.sow_indoor !== "" ? (
           <DetailListItem label="Sowing Indoor" dataText={data.sow_indoor} />
         ) : null}
         {data.sow_outdoor !== "" ? (
           <DetailListItem label="Sowing Outdoor" dataText={data.sow_outdoor} />
         ) : null}
-        <DetailListItem label="Plant Height" dataText={data.height} />
-        <DetailListItem label="Seed Depth" dataText={data.depth} />
-        <DetailListItem label="Seed Spacing" dataText={data.spacing} />
-        <DetailListItem label="Water Requirements" dataText={data.water} />
-        <DetailListItem
-          label="Planting Months"
-          dataText={data.zone._8b.join(", ")}
-        />
-        <DetailListItem
-          label="Nutrient Requirements"
-          dataText={data.nutrient.join(", ")}
-        />
-        <DetailListItem
-          label="Soil Temperature Low"
-          dataText={data.soil_temp_low}
-        />
-        <DetailListItem
-          label="Byproduct"
-          dataText={data.byproducts.join(", ")}
-        />
-        <DetailListItem
-          label="Companion Plants"
-          dataText={data.companions.join(", ")}
-        />
+        {data.height ? (
+          <DetailListItem label="Plant Height" dataText={data.height} />
+        ) : null}
+        {data.depth ? (
+          <DetailListItem label="Seed Depth" dataText={data.depth} />
+        ) : null}
+        {data.spacing ? (
+          <DetailListItem label="Seed Spacing" dataText={data.spacing} />
+        ) : null}
+        {data.water ? (
+          <DetailListItem label="Water Requirements" dataText={data.water} />
+        ) : null}
+        {data.zone._8b ? (
+          <DetailListItem
+            label="Planting Months"
+            dataText={data.zone._8b.join(", ")}
+          />
+        ) : null}
+        {data.nutrient ? (
+          <DetailListItem
+            label="Nutrient Requirements"
+            dataText={data.nutrient.join(", ")}
+          />
+        ) : null}
+        {data.soil_temp_low ? (
+          <DetailListItem
+            label="Soil Temperature Low"
+            dataText={data.soil_temp_low}
+          />
+        ) : null}
+        {data.byproducts ? (
+          <DetailListItem
+            label="Byproduct"
+            dataText={data.byproducts.join(", ")}
+          />
+        ) : null}
+        {data.companions ? (
+          <DetailListItem
+            label="Companion Plants"
+            dataText={data.companions.join(", ")}
+          />
+        ) : null}
         <DetailListItem label="Images" dataText={data.images} />
-        <DetailListItem label="Complete Data" dataText={data.complete} />
+        {data.complete === true ? (
+          <DetailListItem label="Complete Data" dataText="Yes" />
+        ) : (
+          <DetailListItem label="Complete Data" dataText="No" />
+        )}
+        {data.public === true ? (
+          <DetailListItem label="Public Seed" dataText="Yes" />
+        ) : (
+          <DetailListItem label="Public Seed" dataText="No" />
+        )}
         <View
           style={{
             justifyContent: "space-evenly",
