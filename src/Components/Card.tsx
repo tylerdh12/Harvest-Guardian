42ca5aa873937a124cf2212b5e9b586a33c5fd64
Tyler Harper <tylerdh12@gmail.com>
2020-08-12 16:04:37 -0700

    Delete Gesture - Need to refactor

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { Animated, StyleSheet, TouchableOpacity } from "react-native";
 import Swipeable from "react-native-gesture-handler/Swipeable";
@@ -12,14 +13,9 @@ interface CardProps {
 }
 
 const styles = StyleSheet.create({
-  leftAction: {
-    borderRadius: 30,
-    backgroundColor: "#388e3c",
-    justifyContent: "center",
-    flex: 1,
-  },
   rightAction: {
-    borderRadius: 30,
+    borderTopRightRadius: 30,
+    borderBottomRightRadius: 30,
     backgroundColor: "#dd2c00",
     justifyContent: "center",
     flex: 1,
@@ -32,24 +28,6 @@ const styles = StyleSheet.create({
   },
 });
 
-const LeftActions = ({ progress, dragX, onPress }) => {
-  const scale = dragX.interpolate({
-    inputRange: [0, 100],
-    outputRange: [0, 1],
-    extrapolate: "clamp",
-  });
-
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <ViewAlt style={styles.leftAction}>
-        <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
-          Add To Garden
-        </Animated.Text>
-      </ViewAlt>
-    </TouchableOpacity>
-  );
-};
-
 const RightActions = ({ progress, dragX, onPress }) => {
   const scale = dragX.interpolate({
     inputRange: [-100, 0],
@@ -61,7 +39,7 @@ const RightActions = ({ progress, dragX, onPress }) => {
     <TouchableOpacity onPress={onPress}>
       <ViewAlt style={styles.rightAction}>
         <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
-          Delete
+          <Ionicons name="ios-trash" size={70} />
         </Animated.Text>
       </ViewAlt>
     </TouchableOpacity>
@@ -71,7 +49,6 @@ const RightActions = ({ progress, dragX, onPress }) => {
 export const Card: React.FC<CardProps> = ({
   navigation,
   item,
-  onLeftPress,
   onRightPress,
   children,
 }) => (
@@ -82,11 +59,13 @@ export const Card: React.FC<CardProps> = ({
         type: "seed",
       })
     }
+    style={{ backgroundColor: "#dd2c00" }}
   >
     <Swipeable
-      renderLeftActions={(progress, dragX) => (
-        <LeftActions progress={progress} dragX={dragX} onPress={onLeftPress} />
-      )}
+      friction={2}
+      overshootLeft={false}
+      overshootRight={false}
+      rightThreshold={50}
       renderRightActions={(progress, dragX) => (
         <RightActions
           progress={progress}
