d4e678d90daf30c11cd3c4cb66671a5aa3313e2f
Tyler Harper <tylerdh12@gmail.com>
2020-08-12 13:12:57 -0700

    Revert "gesture handling using Reanimated"
    This reverts commit 00f26a2743383d18e9aba3c7158ac4c0b9bf0506.

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,47 +1,36 @@
 import React from "react";
-import {
-  Animated,
-  Dimensions,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import {
-  PanGestureHandler,
-  State,
-  TouchableWithoutFeedback,
-} from "react-native-gesture-handler";
-import {
-  abs,
-  add,
-  call,
-  clockRunning,
-  cond,
-  eq,
-  not,
-  set,
-  useCode,
-} from "react-native-reanimated";
-import {
-  clamp,
-  minus,
-  snapPoint,
-  timing,
-  useClock,
-  usePanGestureHandler,
-  useValue,
-} from "react-native-redash";
-import { HEIGHT, PlantLayout } from "./../components/PlantLayout";
-import Action from "./Actions";
-import { ViewAlt } from "./Styles";
+import { Animated, StyleSheet, TouchableOpacity } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import { CardWrapper, ViewAlt } from "./Styles";
 
 interface CardProps {
   navigation: any;
   item: any;
   onLeftPress?: any;
   onRightPress?: any;
-  onSwipe: () => void;
+  children: any;
 }
+
+const styles = StyleSheet.create({
+  leftAction: {
+    borderRadius: 30,
+    backgroundColor: "#388e3c",
+    justifyContent: "center",
+    flex: 1,
+  },
+  rightAction: {
+    borderRadius: 30,
+    backgroundColor: "#dd2c00",
+    justifyContent: "center",
+    flex: 1,
+    alignItems: "flex-end",
+  },
+  actionText: {
+    color: "#fff",
+    fontWeight: "700",
+    padding: 20,
+  },
+});
 
 const LeftActions = ({ progress, dragX, onPress }) => {
   const scale = dragX.interpolate({
@@ -69,100 +58,44 @@ const RightActions = ({ progress, dragX, onPress }) => {
   });
 
   return (
-    <View style={styles.rightAction}>
-      <TouchableOpacity onPress={onPress}>
-        <ViewAlt style={styles.rightAction}>
-          <Animated.Text
-            style={[styles.actionText, { transform: [{ scale }] }]}
-          >
-            Delete
-          </Animated.Text>
-        </ViewAlt>
-      </TouchableOpacity>
-    </View>
+    <TouchableOpacity onPress={onPress}>
+      <ViewAlt style={styles.rightAction}>
+        <Animated.Text style={[styles.actionText, { transform: [{ scale }] }]}>
+          Delete
+        </Animated.Text>
+      </ViewAlt>
+    </TouchableOpacity>
   );
 };
 
-const { width } = Dimensions.get("window");
-const snapPoints = [-width, -100, 0];
-
-export const Card = ({ navigation, item, onSwipe }: CardProps) => {
-  const {
-    gestureHandler,
-    translation,
-    velocity,
-    state,
-  } = usePanGestureHandler();
-  const translateX = useValue(0);
-  const offsetX = useValue(0);
-  const height = useValue(HEIGHT);
-  const deleteOpacity = useValue(1);
-  const clock = useClock();
-  const to = snapPoint(translateX, velocity.x, snapPoints);
-  const shouldRemove = useValue(0);
-  useCode(
-    () => [
-      cond(
-        eq(state, State.ACTIVE),
-        set(
-          translateX,
-          add(offsetX, clamp(translation.x, -9999, minus(offsetX)))
-        )
-      ),
-      cond(eq(state, State.END), [
-        set(translateX, timing({ clock, from: translateX, to })),
-        set(offsetX, translateX),
-        cond(eq(to, -width), set(shouldRemove, 1)),
-      ]),
-      cond(shouldRemove, [
-        set(height, timing({ from: HEIGHT, to: 0 })),
-        set(deleteOpacity, 0),
-        cond(not(clockRunning(clock)), call([], onSwipe)),
-      ]),
-    ],
-    [onSwipe]
-  );
-  return (
-    <Animated.View>
-      <View style={styles.background}>
-        <TouchableWithoutFeedback onPress={() => shouldRemove.setValue(1)}>
-          <Action x={abs(translateX)} {...{ deleteOpacity }} />
-        </TouchableWithoutFeedback>
-      </View>
-      <PanGestureHandler {...gestureHandler}>
-        <Animated.View style={{ transform: [{ translateX }] }}>
-          <PlantLayout {...{ item }} />
-        </Animated.View>
-      </PanGestureHandler>
-    </Animated.View>
-  );
-};
-
-const styles = StyleSheet.create({
-  background: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "#E1E2E3",
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    overflow: "hidden",
-  },
-  leftAction: {
-    borderRadius: 30,
-    backgroundColor: "#388e3c",
-    justifyContent: "center",
-    flex: 1,
-  },
-  rightAction: {
-    borderRadius: 30,
-    backgroundColor: "#dd2c00",
-    justifyContent: "center",
-    flex: 1,
-    alignItems: "flex-end",
-  },
-  actionText: {
-    color: "#fff",
-    fontWeight: "700",
-    padding: 20,
-  },
-});
+export const Card: React.FC<CardProps> = ({
+  navigation,
+  item,
+  onLeftPress,
+  onRightPress,
+  children,
+}) => (
+  <CardWrapper
+    onPress={() =>
+      navigation.navigate("Details", {
+        data: item,
+        type: "seed",
+      })
+    }
+  >
+    <Swipeable
+      renderLeftActions={(progress, dragX) => (
+        <LeftActions progress={progress} dragX={dragX} onPress={onLeftPress} />
+      )}
+      renderRightActions={(progress, dragX) => (
+        <RightActions
+          progress={progress}
+          dragX={dragX}
+          onPress={onRightPress}
+        />
+      )}
+    >
+      {children}
+    </Swipeable>
+  </CardWrapper>
+);
