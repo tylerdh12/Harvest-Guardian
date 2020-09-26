0581a96411bf4e160d478bd8aef9e4b782d17603
Tyler Harper <tylerdh12@gmail.com>
2020-09-26 14:45:29 -0700

    adding weather outline to my garden

--- src/stacks/MyGarden/MyGarden.tsx
+++ src/stacks/MyGarden/MyGarden.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, RefreshControl } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  RefreshControl,
+} from "react-native";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 import { CardLayout } from "../../components/Card/CardLayout";
-import { SafeAreaView, View } from "../../components/Styles";
+import { SafeAreaView, Text, View, ViewAlt } from "../../components/Styles";
 import { deletePlantAlert, getPlants } from "../../utils/Utils";
 import { RightActionDelete } from "./../../components/Card/RightActionDelete";
 
+const { width } = Dimensions.get("window");
 interface MyGardenProps {
   navigation: any;
 }
@@ -36,42 +42,59 @@ export const MyGarden: React.FC<MyGardenProps> = ({ navigation }) => {
       {isLoading ? (
         <ActivityIndicator size="large" />
       ) : (
-        <FlatList
-          style={{ marginTop: 8 }}
-          refreshControl={
-            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-          }
-          renderItem={({ item, index }) => (
-            <View style={{ margin: 8 }}>
-              <Swipeable
-                friction={2}
-                overshootLeft={false}
-                overshootRight={false}
-                rightThreshold={50}
-                renderRightActions={(progress, dragX) => (
-                  <RightActionDelete
-                    progress={progress}
-                    dragX={dragX}
-                    onPress={() => {
-                      deletePlantAlert({
-                        data: item,
-                        onRefresh: () => onRefresh(),
-                      });
-                    }}
+        <>
+          <ViewAlt
+            style={{
+              marginTop: 14,
+              padding: 10,
+              borderRadius: 15,
+              width: width - 20,
+              alignItems: "center",
+            }}
+          >
+            <Text>Weather Widget</Text>
+            <ViewAlt>
+              
+              <Text>Now</Text>
+            </ViewAlt>
+          </ViewAlt>
+          <FlatList
+            style={{ marginTop: 8 }}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+            renderItem={({ item, index }) => (
+              <View style={{ margin: 8 }}>
+                <Swipeable
+                  friction={2}
+                  overshootLeft={false}
+                  overshootRight={false}
+                  rightThreshold={50}
+                  renderRightActions={(progress, dragX) => (
+                    <RightActionDelete
+                      progress={progress}
+                      dragX={dragX}
+                      onPress={() => {
+                        deletePlantAlert({
+                          data: item,
+                          onRefresh: () => onRefresh(),
+                        });
+                      }}
+                    />
+                  )}
+                >
+                  <CardLayout
+                    {...{ item }}
+                    navigation={navigation}
+                    type="plant"
                   />
-                )}
-              >
-                <CardLayout
-                  {...{ item }}
-                  navigation={navigation}
-                  type="plant"
-                />
-              </Swipeable>
-            </View>
-          )}
-          keyExtractor={(plant: any, idx) => plant + idx}
-          data={data}
-        />
+                </Swipeable>
+              </View>
+            )}
+            keyExtractor={(plant: any, idx) => plant + idx}
+            data={data}
+          />
+        </>
       )}
     </SafeAreaView>
   );
